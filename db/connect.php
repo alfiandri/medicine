51d3813fe4e19fb576015fb21e4df7a4621da3da
Alfiandri Putra Perdana <alfiandri.putra@topremit.com>
2023-12-23 11:47:43 +0700

    add db and services

--- db/connect.php
+++ db/connect.php
@@ -5,3 +5,32 @@ $password = "";
 // $database = "db_simrs";
 $database = "db_antrean";
 $koneksi = mysqli_connect($host, $username, $password, $database);
+
+
+
+function connectToDatabase()
+{
+    global $host;
+    global $username;
+    global $password;
+    global $database;
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (Exception $e) {
+        throw new Exception("Connection failed: " . $$e->getMessage(), 400);
+    }
+}
+
+function executeQuery($connection, $sql, $params = [])
+{
+    try {
+        $statement = $connection->prepare($sql);
+        $statement->execute($params);
+        return $statement;
+    } catch (Exception $e) {
+        throw new Exception("Query failed: " . $e->getMessage(), 400);
+    }
+}
